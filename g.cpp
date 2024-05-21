7fc6df6c73bbc453bd561832170a377954c0bfa5
evanduffield9 <166574917+evanduffield9@users.noreply.github.com>
2024-05-20 18:09:54 -0700

    Create g.cpp

---
+++ g.cpp
@@ -0,0 +1,77 @@
+// Your First C++ Program
+
+#include <iostream> //th
+#include <string>.      // th oasis get function
+#include <fstream>
+using namespace std;
+
+int oisdg;
+
+int y ;
+
+const int x = 25525534;
+
+const int input_length = 1002;
+
+char v2[input_length];
+
+int grab_input()
+{
+    int x = 0;
+
+    char v[input_length];
+    cin.get(v, sizeof(v));
+
+    strncpy(v2,v,input_length);
+
+
+    x++;
+
+    x+=1000;
+
+
+    return x;
+}
+
+int grab_input_from_daemon()
+{
+    int x= 0;
+
+    x++;
+
+    return x;
+
+    //return x/;
+}
+
+int main ()
+{
+  string c;
+  string c2;
+  c ="ine r. 342 3842p2 3io oi";
+  c2= "int 234892 8 780929829ipo2nio pn n2opin oi2oio2 iop ino2 on";
+  int x= grab_input();
+
+  grab_input_from_daemon();
+
+  x++;
+
+  cout << v2 << endl;
+
+  //;;oisdg io pioa npiop inonoi nipo  
+
+  //input 
+  //io.  
+
+  //  as idog opais diop iona ionpdio npan iopg niop nipo23io. 2389 277982798982. io2 io2 io3 
+  
+   int x2 = 0;
+
+   x++;
+
+   cout << x<< endl;
+
+   y++;
+
+   cout << y << endl;
+}
